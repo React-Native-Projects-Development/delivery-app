cd85b856cff644dd175767ff4e74cb1d7f3f57de
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-28 13:55:10 -0600

    add navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,7 @@
+import {
+  NativeStackNavigationProp,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
 import React, {useState} from 'react';
 
 import {
@@ -16,6 +20,7 @@ import {
   initialCurrentLocation,
   restaurantData,
 } from '../data/DUMMY_DATA';
+import {RootStackParams} from '../navigation/AppNavigator';
 
 type HeaderProps = {
   location: string;
@@ -145,7 +150,132 @@ const MainCategories = ({
   );
 };
 
-const Home = () => {
+type RestaurantProps = {
+  restaurants: any;
+  getCategoryNameById: (id: number) => string;
+  navigation: NativeStackNavigationProp<RootStackParams, 'Main'>;
+  currentLocation: any;
+};
+
+const RestaurantList = ({
+  restaurants,
+  getCategoryNameById,
+  navigation,
+  currentLocation,
+}: RestaurantProps) => {
+  const renderItem = ({item}: any) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.9}
+        style={{marginBottom: SIZES.padding * 2}}
+        onPress={() => {
+          navigation.navigate('Restaurant', {
+            item,
+            location: currentLocation,
+          });
+        }}>
+        <View style={{marginBottom: SIZES.padding}}>
+          <Image
+            source={item.photo}
+            resizeMode="cover"
+            style={{width: '100%', height: 200, borderRadius: SIZES.radius}}
+          />
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              height: 50,
+              width: SIZES.width * 0.3,
+              backgroundColor: COLORS.white,
+              borderTopRightRadius: SIZES.radius,
+              borderBottomLeftRadius: SIZES.radius,
+              alignItems: 'center',
+              justifyContent: 'center',
+              ...styles.shadow,
+            }}>
+            <Text style={{...FONTS.h4}}>{item.duration}</Text>
+          </View>
+        </View>
+
+        {/* Restaurant Info */}
+        <Text style={{...FONTS.body2}}>{item.name}</Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginTop: SIZES.padding,
+          }}>
+          {/* Rating */}
+          <Image
+            source={icons.star}
+            style={{
+              height: 20,
+              width: 20,
+              tintColor: COLORS.primary,
+              marginRight: 10,
+            }}
+          />
+          <Text style={{...FONTS.body3}}>{item.rating}</Text>
+          {/* Categories */}
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginLeft: 10,
+            }}>
+            {item.categories.map((category: any) => {
+              return (
+                <View
+                  key={category}
+                  style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={{...FONTS.body3}}>
+                    {getCategoryNameById(category)}
+                  </Text>
+
+                  <Text
+                    style={{...FONTS.h3, color: COLORS.darkGray, bottom: 3}}>
+                    {' '}
+                    .{' '}
+                  </Text>
+                </View>
+              );
+            })}
+
+            {/* Price section */}
+            {[1, 2, 3].map(priceRating => (
+              <Text
+                key={priceRating}
+                style={{
+                  ...FONTS.body3,
+                  color:
+                    priceRating <= item.priceRating
+                      ? COLORS.black
+                      : COLORS.darkGray,
+                }}>
+                $
+              </Text>
+            ))}
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <FlatList
+      data={restaurants}
+      keyExtractor={item => `${item.id}`}
+      renderItem={renderItem}
+      contentContainerStyle={{
+        paddingHorizontal: SIZES.padding * 2,
+        paddingBottom: 30,
+      }}
+    />
+  );
+};
+
+interface Props extends NativeStackScreenProps<RootStackParams, 'Main'> {}
+
+const Home = ({navigation}: Props) => {
   const [categories, setCategories] = useState(categoryData);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [restaurants, setRestaurants] = useState(restaurantData);
@@ -155,12 +285,22 @@ const Home = () => {
 
   const onSelectCategory = (category: any) => {
     // filter restaunrant
-    const restaurantList = restaurants.filter(restaurant =>
+    const restaurantList = restaurantData.filter(restaurant =>
       restaurant.categories.includes(category.id),
     );
 
     setRestaurants(restaurantList);
     setSelectedCategory(category);
+  };
+
+  const getCategoryNameById = (id: number) => {
+    const category = categories.filter(cat => cat.id === id);
+
+    if (category.length > 0) {
+      return category[0].name;
+    } else {
+      return '';
+    }
   };
 
   return (
@@ -170,6 +310,12 @@ const Home = () => {
         categories={categories}
         selectedCategory={selectedCategory}
         onSelectCategory={onSelectCategory}
+      />
+      <RestaurantList
+        restaurants={restaurants}
+        getCategoryNameById={getCategoryNameById}
+        navigation={navigation}
+        currentLocation={currentLocation}
       />
     </SafeAreaView>
   );
