1744dd1d7fc52e52d52c8080ddb5a64bd54bc259
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-28 10:45:47 -0600

    add AppNavigator component

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {OrderDelivery, Restaurant} from '../screens';
+import Tabs from './Tabs';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName="Main">
+      <Stack.Screen name="Main" component={Tabs} />
+      <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      <Stack.Screen name="Restaurant" component={Restaurant} />
+    </Stack.Navigator>
+  );
+};
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
