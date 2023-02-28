91b151275a9dccdc54779cabc24733960d32d871
Brayan Garcia <bgarcia.95@outlook.com>
2023-02-28 13:06:43 -0600

    display categories list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -71,6 +71,80 @@ const Header = ({location}: HeaderProps) => {
   );
 };
 
+type CategoriesProps = {
+  categories: any;
+  selectedCategory: any;
+  onSelectCategory: (category: any) => void;
+};
+
+const MainCategories = ({
+  categories,
+  selectedCategory,
+  onSelectCategory,
+}: CategoriesProps) => {
+  const renderItem = ({item}: any) => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.9}
+        onPress={() => onSelectCategory(item)}
+        style={{
+          padding: SIZES.padding,
+          paddingBottom: SIZES.padding * 2,
+          backgroundColor:
+            selectedCategory?.id === item.id ? COLORS.primary : COLORS.white,
+          borderRadius: SIZES.radius,
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginRight: SIZES.padding,
+          ...styles.shadow,
+        }}>
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 25,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor:
+              selectedCategory?.id === item.id
+                ? COLORS.white
+                : COLORS.lightGray,
+          }}>
+          <Image
+            source={item.icon}
+            resizeMode="contain"
+            style={{width: 30, height: 30}}
+          />
+        </View>
+        <Text
+          style={{
+            marginTop: SIZES.padding,
+            color:
+              selectedCategory?.id === item.id ? COLORS.white : COLORS.black,
+            ...FONTS.body5,
+          }}>
+          {item.name}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{padding: SIZES.padding * 2}}>
+      <Text style={{...FONTS.h1}}>Main</Text>
+      <Text style={{...FONTS.h1}}>Categories</Text>
+      <FlatList
+        data={categories}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        contentContainerStyle={{paddingVertical: SIZES.padding * 2}}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
+};
+
 const Home = () => {
   const [categories, setCategories] = useState(categoryData);
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -79,9 +153,24 @@ const Home = () => {
     initialCurrentLocation,
   );
 
+  const onSelectCategory = (category: any) => {
+    // filter restaunrant
+    const restaurantList = restaurants.filter(restaurant =>
+      restaurant.categories.includes(category.id),
+    );
+
+    setRestaurants(restaurantList);
+    setSelectedCategory(category);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Header location={currentLocation.streetName} />
+      <MainCategories
+        categories={categories}
+        selectedCategory={selectedCategory}
+        onSelectCategory={onSelectCategory}
+      />
     </SafeAreaView>
   );
 };
